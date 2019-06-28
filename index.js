2b6ca3d40963a356450e8dbb292379abf2964430
Chris Hodge <chrishodge87@gmail.com>
2019-06-28 16:56:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-/*
-function takeANumber(katzDeliLine, name){
-var array = []
-for (var i=0; i < katzDeliLine.length; i++){
-array.push("Welcome, " + name[i] + ". You are currently number " + katzDeliLine[i] + " in line")
 
-}
-return array
-
-}
-
-function theBeatlesPlay(musicians, instruments){
-var array = []
-for (var i = 0; i < musicians.length; i++) {
-  array.push(musicians[i]+" plays "+instruments[i])}
-return array;
-}
-*/
 
 function takeANumber (katzDeliLine, name) {
 
