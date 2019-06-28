aeb7f14a1a15e3cbef2a81fcd2ffa655352b24d0
Chris Hodge <chrishodge87@gmail.com>
2019-06-28 14:23:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function takeANumber(katzDeliLine, name){
 var array = []
 for (var i=0; i < array.Length; i++){
 arry.push("Welcome, " + name ". You are currently number " + i " in line")
-  
+
 }
 return array
   
