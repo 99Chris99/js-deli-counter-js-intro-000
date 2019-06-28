928ef6b3106f781ecd4668dd9ef7582ace87fa22
Chris Hodge <chrishodge87@gmail.com>
2019-06-28 14:23:16 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function takeANumber(katzDeliLine, name){
+var array = []
+for (var i=0; i < array.Length; i++){
+arry.push("Welcome, " + name ". You are currently number " + i " in line")
+  
+}
+return array
+  
+}
