7512eeb749c5dd2c0564cf69d00eba97096a1549
Chris Hodge <chrishodge87@gmail.com>
2019-06-28 15:02:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ function takeANumber (katzDeliLine, name) {
 
 katzDeliLine++
 
+var opening = "Welcome, "
+var middle = ". You are number "
+var closing = " in line."
+
 //return "Welcome, "+name+". You are number "+katzDeliLine+" in line."
 return ". You are number "+katzDeliLine+" in line."
 //WHY DOESN'T THIS WORK!  return "Welcome, " + name + ". You are number " + katzDeliLine + " in line."
